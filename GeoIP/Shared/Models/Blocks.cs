6c2f8c6fba1b7248f696f5e0bcfa3406db8e8add
Nikita Neverov <neverovnikita.bmt@gmail.com>
2020-01-19 01:42:13 +0300

    [F] Model added

---
+++ GeoIP/Shared/Models/Blocks.cs
@@ -0,0 +1,28 @@
+﻿#region HEADER
+//   Blocks.cs of GeoIP.Shared
+//   Created by Nikita Neverov at 19.01.2020 0:12
+#endregion
+
+
+using System;
+using System.Net;
+
+
+namespace GeoIP.Shared.Models
+{
+    public class Blocks
+    {
+        public ValueTuple<IPAddress, int> Network { get; set; }
+        public short? GeonameId { get; set; }
+        public short? RegisteredCountryGeonameId { get; set; }
+        public short? RepresentedCountryGeonameId { get; set; }
+        public bool? IsAnonymousProxy { get; set; }
+        public bool? IsSatelliteProvider { get; set; }
+        public string PostalCode { get; set; }
+        public decimal? Latitude { get; set; }
+        public decimal? Longitude { get; set; }
+        public short? AccuracyRadius { get; set; }
+
+        public virtual Locations Geoname { get; set; }
+    }
+}
---
+++ GeoIP/Shared/Models/Locations.cs
@@ -0,0 +1,31 @@
+﻿#region HEADER
+//   Locations.cs of GeoIP.Shared
+//   Created by Nikita Neverov at 19.01.2020 0:12
+#endregion
+
+
+using System.Collections.Generic;
+
+
+namespace GeoIP.Shared.Models
+{
+    public class Locations
+    {
+        public short GeonameId { get; set; }
+        public string LocaleCode { get; set; }
+        public string ContinentCode { get; set; }
+        public string ContinentName { get; set; }
+        public string CountryIsoCode { get; set; }
+        public string CountryName { get; set; }
+        public string Subdivision1IsoCode { get; set; }
+        public string Subdivision1Name { get; set; }
+        public string Subdivision2IsoCode { get; set; }
+        public string Subdivision2Name { get; set; }
+        public string CityName { get; set; }
+        public short? MetroCode { get; set; }
+        public string TimeZone { get; set; }
+        public bool? IsInEuropeanUnion { get; set; }
+
+        public virtual ICollection<Blocks> Blocks { get; set; }
+    }
+}
