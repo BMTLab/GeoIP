9a8ecfb20adc9369d9a3d1fbbf58f883dbd686c7
Nikita Neverov <neverovnikita.bmt@gmail.com>
2020-01-19 22:17:24 +0300

    [F] Added update algorithm

---
+++ GeoIP/Updater/Loader.cs
@@ -0,0 +1,29 @@
+﻿#region HEADER
+//   Loader.cs of GeoIP.Updater
+//   Created by Nikita Neverov at 19.01.2020 17:25
+#endregion
+
+
+using System.ComponentModel;
+using System.Net;
+using System.Threading.Tasks;
+
+
+namespace GeoIP.Updater
+{
+    public static class Loader
+    {
+        public static AsyncCompletedEventHandler? DownloadResultHandler;
+        public static DownloadProgressChangedEventHandler? DownloadProgressHandler;
+        
+        public static async Task LoadAsync(string sourceUrl, string destinationPath)
+        {
+            var webClient = new WebClient();
+
+            webClient.DownloadFileCompleted += DownloadResultHandler;
+            webClient.DownloadProgressChanged += DownloadProgressHandler;
+            
+            await webClient.DownloadFileTaskAsync(sourceUrl, destinationPath);
+        }
+    }
+}
---
+++ GeoIP/Updater/Scripts/geoipdb_update.sql
@@ -0,0 +1,4 @@
+﻿truncate geoipdb.public.blocks      cascade;
+truncate geoipdb.public.locations   cascade;
+copy geoipdb.public.locations   from :p delimiter ',' csv header;
+copy geoipdb.public.blocks      from :v delimiter ',' csv header;
