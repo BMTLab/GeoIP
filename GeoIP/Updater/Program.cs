c6dea53ce423a069474620d7ce4c38a872ce350f
Nikita Neverov <neverovnikita.bmt@gmail.com>
2020-01-19 16:12:52 +0300

    [F] Application update project created

---
+++ GeoIP/Updater/Program.cs
@@ -0,0 +1,19 @@
+﻿#region HEADER
+//   Program.cs of GeoIP.Updater
+//   Created by Nikita Neverov at 19.01.2020 16:10
+#endregion
+
+
+using System;
+
+
+namespace GeoIP.Updater
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
