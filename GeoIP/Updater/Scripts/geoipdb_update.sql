70f517e56a1683429a8b61f11f04910900b32fff
Nikita Neverov <neverovnikita.bmt@gmail.com>
2020-01-20 07:58:53 +0300

    [I] Added comment to update script

--- GeoIP/Updater/Scripts/geoipdb_update.sql
+++ GeoIP/Updater/Scripts/geoipdb_update.sql
@@ -1,4 +1,7 @@
-﻿truncate geoipdb.public.blocks      cascade;
+﻿-- Update script. 
+-- Use 'p' param to path to locations csv file, 'v' param to path to blocks csv file
+
+truncate geoipdb.public.blocks      cascade;
 truncate geoipdb.public.locations   cascade;
 copy geoipdb.public.locations   from :p delimiter ',' csv header;
 copy geoipdb.public.blocks      from :v delimiter ',' csv header;
