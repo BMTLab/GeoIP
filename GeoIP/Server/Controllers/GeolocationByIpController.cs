7d181e2642d0754642ba16ed97b1feb11b8b376a
Nikita Neverov <neverovnikita.bmt@gmail.com>
2020-01-20 13:09:04 +0300

    [F] Controller trial action added. Аdded request filter to handle erroneous requests

--- GeoIP/Server/Controllers/GeolocationByIpController.cs
+++ GeoIP/Server/Controllers/GeolocationByIpController.cs
@@ -4,6 +4,9 @@
 #endregion
 
 
+using GeoIP.Server.Filters;
+using GeoIP.Server.Services.DataProviders;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -11,26 +14,38 @@ using Microsoft.Extensions.Logging;
 namespace GeoIP.Server.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
     public sealed class GeolocationByIpController : ControllerBase
     {
         #region Fields
+        private readonly IGeoIpProvider _provider;
         private readonly ILogger<GeolocationByIpController> _logger;
         #endregion
-
-
+        
+        
         #region Constructors
         public GeolocationByIpController
         (
+            IGeoIpProvider provider,
             ILogger<GeolocationByIpController> logger
         )
         {
+            _provider = provider;
             _logger = logger;
         }
         #endregion
-
-
+        
+        
         #region Methods.HTTP
+        [HttpGet("{ip}")]
+        [ValidateRequest]
+        public IActionResult Get(string ip)
+        {
+            var y = _provider.GetAllInfoByIp(ip);
+
+            return new JsonResult(new { Y = y?.Location.CityName });
+        }
         #endregion _Methods.HTTP
     }
 }
---
+++ GeoIP/Server/Filters/ValidateModelAttribute.cs
@@ -0,0 +1,23 @@
+﻿#region HEADER
+//   ValidateModelAttribute.cs of GeoIP.Server
+//   Created by Nikita Neverov at 20.01.2020 9:06
+#endregion
+
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+
+namespace GeoIP.Server.Filters
+{
+    public class ValidateRequestAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(context.ModelState);
+            }
+        }
+    }
+}
