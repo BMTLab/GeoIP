a015cddeb38e4c5ef3ff4e73f6733ab4130b10c4
Nikita Neverov <neverovnikita.bmt@gmail.com>
2020-01-20 14:07:12 +0300

    [B] Bug fixed: an interface has been implemented for creating a context instance for eliminating an error in which it is impossible to simultaneously use the database tools and the dbcontext pool

---
+++ GeoIP/Server/Data/GeoIpDbContextFactory.cs
@@ -0,0 +1,34 @@
+﻿#region HEADER
+//   GeoIpDbContextFactory.cs of GeoIP.Server
+//   Created by Nikita Neverov at 20.01.2020 13:56
+#endregion
+
+
+using System.IO;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+
+namespace GeoIP.Server.Data
+{
+    public sealed class GeoIpDbContextFactory : IDesignTimeDbContextFactory<GeoIpDbContext>
+    {
+        public GeoIpDbContext CreateDbContext(string[] args)
+        {
+            var connection = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile(@"Properties/appSettings.json")
+                            .Build()
+                            .GetConnectionString(@"Default");
+
+
+            var options = new DbContextOptionsBuilder<GeoIpDbContext>()
+                         .UseNpgsql(connection)
+                         .Options;
+
+            return new GeoIpDbContext(options);
+        }
+    }
+}
